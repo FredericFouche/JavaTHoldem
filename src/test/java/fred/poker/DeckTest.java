61dadc0e18cd0c8c9ff4c545bc3d230ff51cbb10
Frédéric Fouché <frederic.fouchedm@gmail.com>
2025-02-11 14:21:28 +0100

    feature : Created Deck Class and added 2 methods (draw and shuffle) with tests

--- src/test/java/fred/poker/DeckTest.java
+++ src/test/java/fred/poker/DeckTest.java
@@ -3,9 +3,7 @@ package fred.poker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,6 +23,42 @@ class DeckTest {
         assertEquals(52, fullDeck.size());
         Set<Card> uniqueCards = new HashSet<>(fullDeck);
         assertEquals(52, uniqueCards.size());
+    }
+
+    @Test
+    void draw() {
+        // Tirer 5 cartes avec la méthode draw
+        List<Card> drawnDeck = deck.draw((byte) 5);
+        // Vérifier que le deck a 47 cartes restantes et que les 5 cartes tirées sont bien dans la main
+        assertNotNull(drawnDeck);
+        assertEquals(5, drawnDeck.size());
+        assertEquals(47, deck.deck.size());
+    }
+
+    @Test
+    void shuffle() {
+        List<Card> originalDeck = new ArrayList<>(deck.deck);
+        deck.shuffle();
+        List<Card> shuffledDeck = deck.deck;
+
+        // Vérifier que le deck a été mélangé
+        assertNotEquals(originalDeck, shuffledDeck);
+
+        // Afficher les valeurs des cartes dans les listes
+        System.out.println("Original deck:");
+        for (Card card : originalDeck) {
+            System.out.println(card.getCard());
+        }
+
+        System.out.println("Shuffled deck:");
+        for (Card card : shuffledDeck) {
+            System.out.println(card.getCard());
+        }
+
+        // Vérifier que le deck contient toujours les mêmes cartes
+        originalDeck.sort(Comparator.comparing(Card::toString));
+        shuffledDeck.sort(Comparator.comparing(Card::toString));
+        assertEquals(originalDeck, shuffledDeck);
     }
 
 }
